976123fed4cac511c6046e94fb04a3c5cb69c47f
angeloRobson <angelorobsonmelo@gmail.com>
2019-05-24 15:35:09 -0300

    test deploy

--- app/src/main/java/com/angelorobson/bitrisetest/ui/login/LoginActivity.kt
+++ app/src/main/java/com/angelorobson/bitrisetest/ui/login/LoginActivity.kt
@@ -72,6 +72,7 @@ class LoginActivity : AppCompatActivity() {
         }
 
         password.apply {
+
             afterTextChanged {
                 loginViewModel.loginDataChanged(
                     username.text.toString(),
